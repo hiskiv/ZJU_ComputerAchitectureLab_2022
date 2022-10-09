a259dc84625059b14713074da7c97ee13bc50899
hiski <hiskivv@gmail.com>
2022-10-09 22:45:54 +0800

    lab1 update

---
+++ lab0/core/ROM_D.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+
+module ROM_D(
+    input[6:0] a,
+    output[31:0] spo
+);
+
+    reg[31:0] inst_data[0:127];
+
+    initial	begin
+        $readmemh("D:\\COP\\CA-Exp1\\code\\core\\rom.hex", inst_data);
+        // $readmemh("D:\\COP\\CA-Exp1\\code\\core\\rom.hex", inst_data);
+    end
+
+    assign spo = inst_data[a];
+
+endmodule
